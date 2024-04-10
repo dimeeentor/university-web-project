2034af3e5a3f8d27264593d8019f07ff42add37b
Dmytro Honcharenko <dimeeentor@gmail.com>
2024-04-10 21:45:20 +0200

    updated dialog and added site map

--- scripts/home/home.js
+++ scripts/home/home.js
@@ -1,12 +1,36 @@
-const films = document.querySelectorAll(".film")
-const dialoges = document.querySelectorAll("dialog")
+const films = document.querySelector(".film")
+const closeModals = document.querySelectorAll(".close-modal")
+const dialogs = document.querySelectorAll("dialog")
+const showBtns = document.querySelectorAll(".show-details")
 
-films.forEach((film) => {
-  film.addEventListener("click", () => {
-    film.lastElementChild.showModal()
+showBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    dialogs[index].showModal()
+    document.body.style.overflow = "hidden"
   })
 })
 
-dialoges.forEach((dialog) => {
-  dialog.addEventListener("click", () => {})
+dialogs.forEach((dialog) => {
+  dialog.addEventListener("click", (event) => {
+    if (event.target === dialog) {
+      dialog.close()
+      document.body.style.overflow = "auto"
+    }
+  })
+})
+
+dialogs.forEach((dialog) => {
+  dialog.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      dialog.close()
+      document.body.style.overflow = "auto"
+    }
+  })
+})
+
+closeModals.forEach((closeModal) => {
+  closeModal.addEventListener("click", () => {
+    closeModal.parentElement.close()
+    document.body.style.overflow = "auto"
+  })
 })
